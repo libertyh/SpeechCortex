99c33cf4d522666699dbc084787a53fa6ac1b538
Liberty Hamilton <libertyhamilton@gmail.com>
2021-08-19 14:25:09 -0500

    Added html page title

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 suppress_callback_exceptions=True
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title('Speech Brain Viewer')
 server = app.server
 cache = Cache(app.server, config={
         'CACHE_TYPE': 'redis',
